053f0270bdf17658db02683d3b39bac8e2fa3a81
XardoX <jurek.gab.jg@gmail.com>
2022-10-04 11:57:41 +0200

    lerping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
@@ -14,6 +15,14 @@ public class Player : MonoBehaviour
     bool isGrounded;
     [SerializeField]
     float moveInput;
+    [SerializeField]
+    float lerpDuration = 0.1f;
+
+    [SerializeField]
+    float distance;
+
+    Vector3 newPosition, lerpStartPos;
+    float lerpTime = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +33,29 @@ public class Player : MonoBehaviour
         //transform.position += transform.forward * Time.deltaTime * moveSpeed;
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
 
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.position += -transform.right;
+            lerpStartPos = transform.position;
+            newPosition = transform.position + (-Vector3.right * distance);
+            lerpTime = 0f;
         } else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.position += transform.right;
+            lerpStartPos = transform.position;
+            newPosition = transform.position + (Vector3.right * distance);
+            lerpTime = 0f;
         }
+
+
+        if(lerpTime >= lerpDuration)
+        {
+        } else{
+            lerpTime += Time.deltaTime;
+        }
+
+
+        transform.position = new Vector3( Vector3.Lerp(lerpStartPos, newPosition, lerpTime).x, transform.position.y, transform.position.x);
+
     }
     void FixedUpdate()
     {
